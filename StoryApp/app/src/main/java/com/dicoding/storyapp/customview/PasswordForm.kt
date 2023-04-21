6a6e3845c6942e17b7014e0df7f24a6f6d773444
Xenovka <wendyanto18@gmail.com>
2023-04-21 19:24:52 +0700

    create login and register page

--- StoryApp/app/src/main/java/com/dicoding/storyapp/customview/PasswordForm.kt
+++ StoryApp/app/src/main/java/com/dicoding/storyapp/customview/PasswordForm.kt
@@ -1,4 +1,53 @@
-package com.dicoding.storyapp.customView
+package com.dicoding.storyapp.customview
 
-class EditText {
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.dicoding.storyapp.R
+
+class PasswordForm: AppCompatEditText {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        hint = "Password"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    private fun init() {
+        addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if(s.length < 8) {
+                    background = ContextCompat.getDrawable(context, R.drawable.edit_text_border_warning)
+                    Toast.makeText(context, "password must be contain 8 characters or more", Toast.LENGTH_SHORT).show()
+                } else {
+                    background = ContextCompat.getDrawable(context, R.drawable.edit_text_border)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+    }
 }
--- StoryApp/app/src/main/java/com/dicoding/storyapp/view/register/Register.kt
+++ StoryApp/app/src/main/java/com/dicoding/storyapp/view/register/Register.kt
@@ -2,6 +2,7 @@ package com.dicoding.storyapp.view.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.storyapp.R
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

6aaccb8a0553f885b4b3b5ad48dc1a57b9302b63
Xenovka <wendyanto18@gmail.com>
2023-04-23 13:49:23 +0700

    adding all responses

--- StoryApp/app/src/main/java/com/dicoding/storyapp/api/ApiResponses.kt
+++ StoryApp/app/src/main/java/com/dicoding/storyapp/api/ApiResponses.kt
@@ -1,6 +1,8 @@
 package com.dicoding.storyapp.api
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
 data class StoryUploadResponse(
     @field:SerializedName("error")
@@ -32,10 +34,59 @@ data class LoginResult(
     val token: String
 )
 
-data class LoginRequest(
-    @field:SerializedName("email")
-    val email: String,
+data class RegisterResponse(
+    @field:SerializedName("error")
+    val error: Boolean,
 
-    @field:SerializedName("password")
-    val password: String
+    @field:SerializedName("message")
+    val message: String
 )
+
+@Parcelize
+data class StoryResponse(
+
+    @field:SerializedName("listStory")
+    val listStory: List<StoryItem?>? = null,
+
+    @field:SerializedName("error")
+    val error: Boolean? = null,
+
+    @field:SerializedName("message")
+    val message: String? = null
+) : Parcelable
+
+data class StoryDetailResponse(
+    @field:SerializedName("error")
+    val error: Boolean? = null,
+
+    @field:SerializedName("message")
+    val message: String? = null,
+
+    @field:SerializedName("story")
+    val story: StoryItem
+)
+
+@Parcelize
+data class StoryItem(
+
+    @field:SerializedName("photoUrl")
+    val photoUrl: String? = null,
+
+    @field:SerializedName("createdAt")
+    val createdAt: String? = null,
+
+    @field:SerializedName("name")
+    val name: String? = null,
+
+    @field:SerializedName("description")
+    val description: String? = null,
+
+    @field:SerializedName("lon")
+    val lon: Float? = null,
+
+    @field:SerializedName("id")
+    val id: String? = null,
+
+    @field:SerializedName("lat")
+    val lat: Float? = null
+) : Parcelable

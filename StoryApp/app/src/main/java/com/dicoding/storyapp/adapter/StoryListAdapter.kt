6c7806028b78a1afa33c017a2caaaef4e92c5307
Xenovka <wendyanto18@gmail.com>
2023-04-28 14:14:16 +0700

    removing unused imports

--- StoryApp/app/src/main/java/com/dicoding/storyapp/adapter/StoryListAdapter.kt
+++ StoryApp/app/src/main/java/com/dicoding/storyapp/adapter/StoryListAdapter.kt
@@ -1,13 +1,11 @@
 package com.dicoding.storyapp.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.dicoding.storyapp.R
 import com.dicoding.storyapp.api.StoryItem
 import com.dicoding.storyapp.databinding.StoryItemBinding
 

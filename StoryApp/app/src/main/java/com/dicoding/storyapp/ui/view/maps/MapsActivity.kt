b7e08f7adbd84ca8d59ae92b92031702b541a4e6
Xenovka <wendyanto18@gmail.com>
2023-05-14 12:12:19 +0700

    applying retro mapstyle and showing markers

--- StoryApp/app/src/main/java/com/dicoding/storyapp/ui/view/maps/MapsActivity.kt
+++ StoryApp/app/src/main/java/com/dicoding/storyapp/ui/view/maps/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.dicoding.storyapp.ui.view.maps
 
 import android.content.Context
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,7 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.storyapp.R
+import com.dicoding.storyapp.api.StoryItem
 import com.dicoding.storyapp.databinding.ActivityMapsBinding
 import com.dicoding.storyapp.model.UserPreference
 import com.dicoding.storyapp.ui.view.ViewModelFactory
@@ -18,6 +20,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "data")
@@ -62,7 +65,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
 
-        // Add a marker in Sydney and move the camera
+        setMapStyle()
+        viewModel.getStories().observe(this) {
+            addManyMarkers(it)
+        }
 
     }
 
@@ -73,5 +79,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         )[MapsViewModel::class.java]
 
         viewModel.setStories()
+    }
+
+    private fun setMapStyle() {
+        try {
+            val success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
+            if(!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (exception: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", exception)
+        }
+    }
+
+    private fun addManyMarkers(data: ArrayList<StoryItem>) {
+        val stories = data.toList()
+        stories.forEach { story ->
+            if(story.lat != null && story.lon != null) {
+                val latLng = LatLng(story.lat.toDouble(), story.lon.toDouble())
+                mMap.addMarker(MarkerOptions().position(latLng).title(story.name))
+            }
+
+        }
+    }
+
+    companion object {
+        private const val TAG = "MapsActivity"
     }
 }
